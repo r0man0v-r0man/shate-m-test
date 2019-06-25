6a64c148df964ced03e48dc98c5b892cedbd2ca7
Roman <romanovmailbox@mail.ru>
2019-06-25 19:16:22 +0300

    Add project files.

---
+++ shate-m-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shate_m_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
