3b90f7c6c1c798742e8ac04e7cfab713a23441ae
r0man0v r0man <romanovmailbox@mail.ru>
2019-06-26 12:03:49 +0300

    fix adding brands

--- shate-m-test/wwwroot/js/ShateMTest.js
+++ shate-m-test/wwwroot/js/ShateMTest.js
@@ -11,13 +11,17 @@ ShateMTestConnection.start()
     .catch(error => {
         console.error(error.message);
     });
-submitAddBrand.onclick = function () {
+submitAddBrand.onclick = function (event) {
     var newBrand = document.getElementById("brand").value;
     ShateMTestConnection.invoke("AddBrand", newBrand)
         .catch(function (err) {
             return console.error(err.toString());
         });
+    event.preventDefault();
 };
 ShateMTestConnection.on("AddBrand", function (brandName) {
-    alert(brandName);
+    var li = document.createElement("li");
+    li.classList.add("list-group-item", "list-group-item-primary");
+    li.innerText = brandName;
+    document.getElementById("car-brand-list").appendChild(li);
 });
