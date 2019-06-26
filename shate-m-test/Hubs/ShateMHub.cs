4dfdb18852782b34627bde4d0e42498ae5d85f2a
r0man0v r0man <romanovmailbox@mail.ru>
2019-06-26 11:49:29 +0300

    fix hub

--- shate-m-test/Hubs/ShateMHub.cs
+++ shate-m-test/Hubs/ShateMHub.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using shate_m_test.DAL;
+using shate_m_test.DAL.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,26 @@ namespace shate_m_test.Hubs
 {
     public class ShateMHub : Hub
     {
-
+        private readonly ShateMContext context;
+        public ShateMHub(ShateMContext context)
+        {
+            this.context = context;
+        }
+        public async Task AddBrand(string newBrand)
+        {
+            var newCarBrand = new Brand
+            {
+                Name = newBrand
+            };
+            await context.Brands.AddAsync(newCarBrand);
+            await context.SaveChangesAsync();
+            var test = GetName(context.Brands.FirstOrDefault(c => c.Name == newCarBrand.Name));
+            await Clients.All.SendAsync("AddBrand", test);
+        }
+        public string GetName(Brand brand)
+        {
+            var result = context.Brands.FirstOrDefault(c=>c.Name == brand.Name);
+            return result.Name;
+        }
     }
 }
--- shate-m-test/wwwroot/js/ShateMTest.js
+++ shate-m-test/wwwroot/js/ShateMTest.js
@@ -1,3 +1,23 @@
 ﻿"use strict";
 var ShateMTestConnection = new signalR.HubConnectionBuilder().withUrl("/ShateMHub").build();
 
+var submitAddBrand = document.getElementById("add-brand-submit");
+
+
+ShateMTestConnection.start()
+    .then(function () {
+        console.log("connection addbrand started");
+    })
+    .catch(error => {
+        console.error(error.message);
+    });
+submitAddBrand.onclick = function () {
+    var newBrand = document.getElementById("brand").value;
+    ShateMTestConnection.invoke("AddBrand", newBrand)
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+};
+ShateMTestConnection.on("AddBrand", function (brandName) {
+    alert(brandName);
+});
